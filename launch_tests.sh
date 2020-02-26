31834f6e87c19fcd91c21a0f452b927d95ad45fa
Anna Pershyna <ny4koo@gmail.com>
2020-02-26 01:14:40 +0100

    Delete launch_tests.sh

--- launch_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source ~/.bashrc
-PAGE=https://${SQUASH_DOMAIN} envsubst < test.template.js > test.js
-xvfb-run testcafe --skip-js-errors firefox test.js
-xvfb-run testcafe --skip-js-errors chrome test.js
